aa3033fbfb7d858851b1494adc79377dfc52e343
Mapma3eka262 <vlaskofanov@gmail.com>
2022-06-09 12:28:55 +0500

    HW_lesson4 (09.06.2022)

---
+++ test/com/basejava/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,20 @@
+package com.basejava.webapp.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayStorageTest {
+
+    @Test
+    public void findIndex() {
+    }
+
+    @Test
+    public void deleteResumeFromArray() {
+    }
+
+    @Test
+    public void saveResumeToArray() {
+    }
+}
---
+++ test/com/basejava/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,20 @@
+package com.basejava.webapp.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortedArrayStorageTest {
+
+    @Test
+    public void findIndex() {
+    }
+
+    @Test
+    public void deleteResumeFromArray() {
+    }
+
+    @Test
+    public void saveResumeToArray() {
+    }
+}
